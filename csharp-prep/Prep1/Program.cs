d21229120cad22141e5cf8db8ed5746987d54b67
Ibrahim Kamara <ibrahamacademic@gmail.com>
2024-02-29 14:11:40 +0000

    Complete prep1, prompting for first and last names and display them on screen.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Prompt for first name and store in a firstName variable.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Prompt for last name and store it in a lastName variable
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine();
+
+        // Display the User's name on the screen
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
