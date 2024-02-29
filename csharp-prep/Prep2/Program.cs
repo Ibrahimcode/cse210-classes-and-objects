295de3b68857669c9cc6ca5a06d206367f1618aa
Ibrahim Kamara <ibrahamacademic@gmail.com>
2024-02-29 15:34:35 +0000

    Complete prep1, prompt student for their grade and output their grade letter and performance message.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Prompt students for their grade percentage.
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+
+        double percentage = double.Parse(gradePercentage); // Conver to double
+
+        string gradeLetter = ""; // Will hold the grade lette (A, B, C ...)
+
+        // Round the percentage and cast it to and int then find the extract the last digit.
+        int lastDigit = (int)Math.Round(percentage) % 10;
+
+        string gradeSign = ""; // Will hold the grade sign (+, -)
+
+        // Will not display sign for "F" and "A+";
+        if (percentage >= 60 & percentage < 93)
+        {
+            if (lastDigit >= 7)
+            {
+                gradeSign = "+";
+            }else if (lastDigit < 3)
+            {
+                gradeSign = "-";
+            }else
+            {
+                gradeSign = "";
+            }
+            
+        }
+
+        // Determine the grade letter.
+        if (percentage >= 90)
+        {
+            gradeLetter = "A";
+        }else if (percentage >= 80)
+        {
+            gradeLetter = "B";
+        }else if(percentage >= 70)
+        {
+            gradeLetter = "C";
+        }else if (percentage >= 60)
+        {
+            gradeLetter = "D";
+        }else
+        {
+            gradeLetter = "F";
+        }
+
+        // Output the grade letter and sign.
+        Console.WriteLine($"Your letter grade is {gradeLetter}{gradeSign}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine($"Congratulations! you pass with a grade of {percentage}%");
+        }else
+        {
+            Console.WriteLine("You efforts counts a lot, please take this course again.");
+        }
     }
 }
