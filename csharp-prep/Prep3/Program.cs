22e1161b8f863100f648c55185e2ae4264a16ff7
Ibrahim Kamara <ibrahamacademic@gmail.com>
2024-03-01 17:47:38 +0000

    Complete prep3, number guess game.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        string playAgain = "yes";
+        while (playAgain == "yes")
+        {
+            // Create an instance of an random generator,
+            // and generate an random number.
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+
+            int userGuess = 0; // What the user guessed
+            int count = 0; // Count of user guesses.
+            do
+            {
+
+                Console.Write("What is your guess? ");
+                string guess = Console.ReadLine();
+                userGuess = int.Parse(guess);
+
+                count++;
+
+                if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {count} guess to guessed it right.");
+
+                    Console.WriteLine();
+
+                    Console.Write("Do you want to play again? (yes or  no) ");
+
+                    playAgain  = Console.ReadLine(); // Ask the user if they want to play again.
+                    
+                }
+            } while (userGuess != magicNumber);
+        }
+        
+
+
     }
 }
