a824577aac81e4e5d633704bbbf8f391b712cb17
Ibrahim Kamara <ibrahamacademic@gmail.com>
2024-03-30 17:23:08 +0000

    Add Checklist goal class.

---
+++ prove/Develop05/ChechlistGoal.cs
@@ -0,0 +1,40 @@
+class ChechlistGoal : Goal
+{
+    protected int _amountCompleted;
+    protected int _target;
+    protected int _bonus;
+
+    public ChechlistGoal(
+        string shortName, string description, int pointsValue, int target, int bonus
+    ) : base(shortName, description, pointsValue){
+        this._target = target;
+        this._bonus = bonus;
+    }
+
+    protected override void RecordEvent()
+    {
+        this._accumulatedPoints += this._pointsValue;
+        this._amountCompleted += 1;
+        Console.WriteLine($"Congratulations! you have earned {this._pointsValue} points");
+        Console.WriteLine($"You now have {this._accumulatedPoints} points");
+    }
+
+    protected override bool IsCompleted()
+    {
+        return this._target == this._amountCompleted;
+    }
+
+    protected override GoalData GetGoalData()
+    {
+        return new ChecklistGoalData{
+            goalType = "Simple",
+            shortName = _shortName,
+            description = _description,
+            pointsValue = _pointsValue,
+            accumulatedPoints = _accumulatedPoints,
+            amountCompleted = this._amountCompleted,
+            target = this._target,
+            bonus = this._bonus
+        };
+    }
+}
