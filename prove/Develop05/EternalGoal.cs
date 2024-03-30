7045722a01a3193e94ba811f14faa312f52f3fff
Ibrahim Kamara <ibrahamacademic@gmail.com>
2024-03-30 16:55:18 +0000

    Add Eternal goal class.

---
+++ prove/Develop05/EternalGoal.cs
@@ -0,0 +1,26 @@
+class EternalGoal : Goal
+{
+    public EternalGoal(
+        string shortName, string description, int pointsValue
+    ) : base(shortName, description, pointsValue){
+
+    }
+
+    protected override void RecordEvent()
+    {
+        this._accumulatedPoints += this._pointsValue;
+        Console.WriteLine($"Congratulations! you have earned {this._pointsValue} points");
+        // Console.WriteLine($"You now have {this._accumulatedPoints} points");
+    }
+
+    protected override GoalData GetGoalData()
+    {
+        return new SimpleGoalData{
+            goalType = "Eternal",
+            shortName = _shortName,
+            description = _description,
+            pointsValue = _pointsValue,
+            accumulatedPoints = _accumulatedPoints
+        };
+    }
+}
